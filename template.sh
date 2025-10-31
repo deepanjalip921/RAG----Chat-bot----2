d4de8b3053b49a837503805facf2c20177f0a101
Disha <deepanjalip921@gmail.com>
2025-10-31 06:22:28 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,13 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompts.py
+touch .env
+touch app.py
+touch setup.py
+touch research/trials.ipynb
+touch requiremnets.txt
+
+echo "Directory and files created successfully."
